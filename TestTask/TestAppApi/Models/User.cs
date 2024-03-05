2a1adb7e6c41dd6b63ec4270316ad29c04eb1e76
Vladyslav Kaliuzhnyi <kaliuzhnyi.vladislav@gmail.com>
2024-03-06 00:17:58 +0100

    add User model and IStudyGroupRepository

---
+++ TestTask/TestAppApi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace TestAppApi.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public int UserId { get; set; }
+        public User(string name, int userId)
+        {
+            UserName = name;
+            UserId= userId;
+        }
+    }
+}
---
+++ TestTask/TestAppApi/Repos/IStudyGroupRepository.cs
@@ -0,0 +1,14 @@
+﻿using TestAppApi.Models;
+
+namespace TestAppApi.Repos
+{
+    public interface IStudyGroupRepository
+    {
+        Task CreateStudyGroup(StudyGroup studyGroup);
+        Task<IEnumerable<StudyGroup>> GetStudyGroups();
+        Task<IEnumerable<StudyGroup>> SearchStudyGroups(string subject);
+        Task JoinStudyGroup(int studyGroupId, int userId);
+        Task LeaveStudyGroup(int studyGroupId, int userId);
+        Task<bool> IsUserPresentInGroup(int studyGroupId, int userId);
+    }
+}
