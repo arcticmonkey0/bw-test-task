953e5871613062f8d78352060232f811fb2a7c7a
Vladyslav Kaliuzhnyi <kaliuzhnyi.vladislav@gmail.com>
2024-03-11 11:12:57 +0100

    re-adding missing changes that accidentally made their way into gitignore

---
+++ TestTask/TestTask/Api/Models/CreateStudyGroupRequest.cs
@@ -0,0 +1,18 @@
+﻿using TestAppApi.Models;
+
+namespace TestTask.Api.Models
+{
+    internal class CreateStudyGroupRequest
+    {
+        public CreateStudyGroupRequest(string groupName, string subject, List<User> users)
+        {
+            GroupName = groupName;
+            Subject = subject;
+            Users = users;
+        }
+
+        public string GroupName { get; }
+        public string Subject { get; }
+        public List<User> Users { get; }
+    }
+}
